d6ff1578e9e0970493ebc116ad5eadd97866972b
John Tajima <manjiro@gmail.com>
2013-12-07 20:52:58 -0500

    add files

---
+++ README.md
@@ -0,0 +1,106 @@
+# Gridly.js
+
+A simple jQuery plugin for arranging div elements into a grid.
+
+Create a simple grid layout that is either 
+- fixed width (generally 1920x1080 for use on a HDTV layout)
+- relative (scales in size, best suited for desktop)
+
+### usage
+
+1. Include `gridly.js` in your page. Ensure you have `jquery` loaded before as well.
+
+
+2. Create a html structure like:
+
+```
+<div class='gridly' id='mygrid' data-rows='4' data-cols='6' data-width='1920' data-height='1080' data-orientation='horizontal'>
+  <!-- cell in row 1, col 1, width of 1x and height of 1x -->
+  <div class='cell' data-cell-x='1' data-cell-y='1' data-cell-w='1' data-cell-h='1'>
+   your content
+  </div>
+
+  <!-- cell in row 1, cols 2-3, width of 2x and heigh of 1 -->
+  <div class='cell' data-cell-x='2' data-cell-y='1' data-cell-w='2' data-cell-h='1'>
+   your content
+  </div>
+
+  <!-- a 2x2 cell in row 1, col 4 -->
+  <div class='cell' data-cell-x='4' data-cell-y='1' data-cell-w='2' data-cell-h='2'>
+   your content
+  </div>
+
+  ...
+  ...
+</div>
+```
+
+
+3. Initialize your grid with:
+
+```
+  $('#mygrid').gridly();
+```
+
+#### options
+
+<table>
+  <tr>
+    <th>option</th>
+    <th>default</th>
+    <th>notes</th>
+  </tr>
+  <tr>
+    <td>width</td>
+    <td>1920</td>
+    <td>Width of grid in px</td>
+  </tr>
+  <tr>
+    <td>height</td>
+    <td>1080</td>
+    <td>Height of grid in px</td>
+  </tr>
+  <tr>
+    <td>orientation</td>
+    <td>horizontal</td>
+    <td>'horizontal' or 'vertical'</td>
+  </tr>
+  <tr>
+    <td>rows</td>
+    <td>4</td>
+    <td>number of rows in the grid</td>
+  </tr>
+  <tr>
+    <td>cols</td>
+    <td>6</td>
+    <td>number of columns in the grid</td>
+  </tr>
+  <tr>
+    <td>fixed</td>
+    <td>true</td>
+    <td>true|false - if true, the grid is a fixed size in px. If false, the grid will fix in the browser window and will resize as the window resizes, but keeping it's original ratio.</td>
+  </tr>
+  <tr>
+    <td>gutter</td>
+    <td>0</td>
+    <td>Gutter width between cells. If 0, no gutter. Otherwise gutter values > 0 will create a Xpx width around each cell. The outside border will be the 2xgutter value (ie: there will be each gutter around each cell).
+
+  For example, if you specify gutter=2, then there is going to be a 4px border around the entire dashboard, and a 4px between each cell.
+    </td>
+  </tr>
+</table>
+
+You can specify the options in either a hash when you initialize the grid
+
+```
+$('#mygrid').gridly({width:1920, rows:4, cols:6})
+```
+
+or you can specify `data-` attributes in the `div` element of the grid.
+
+
+
+### demo pages
+
+- fixed layout
+- relative
---
+++ src/gridly.js
@@ -0,0 +1,144 @@
+/* 
+ * gridly.js
+ * by John Tajima, Copyright 2013
+ * MIT License
+ *
+ * A simple jQuery plugin for arranging div elements into a grid
+ *
+ * 
+ */
+
+(function($){
+
+  $.fn.gridly = function(options) {
+    var curr_options = {}
+    curr_options['rows']        = $(this).data('rows');
+    curr_options['cols']        = $(this).data('cols');
+    curr_options['width']       = $(this).data('width');
+    curr_options['height']      = $(this).data('height');
+    curr_options['orientation'] = $(this).data('orientation');
+    curr_options['fixed']       = $(this).data('fixed');
+    curr_options['gutter']      = $(this).data('gutter');
+
+    $.fn.gridly.opts      = $.extend({}, $.fn.gridly.defaults, curr_options, options);
+    $.fn.gridly.opts.grid = this;
+
+    // set resize listener on window
+    $(window).on('resize', resizeGrid);
+    console.log($.fn.gridly.opts)
+    resizeGrid();
+
+    return this;
+  };
+
+  // defaults
+  $.fn.gridly.defaults = {
+    width: 1920,
+    height: 1080,
+    rows: 4,
+    cols: 6,
+    orientation: 'horizontal',
+    fixed: true,   // fixed size by pixel
+    gutter: 0
+  };
+
+  // current option values
+  $.fn.gridly.opts = {}
+
+  // private methods
+  // --------------------
+
+  // load pins into vertical columns
+  function resizeGrid() {
+    var grid        = $.fn.gridly.opts.grid;
+    var width       = $.fn.gridly.opts.width;
+    var height      = $.fn.gridly.opts.height;
+    var curr_width  = $(grid).outerWidth();
+    var curr_height = $(grid).outerHeight();
+    var vertical    = ($.fn.gridly.opts.orientation === 'vertical');
+    
+    // only resize if not fixed
+    if ($.fn.gridly.opts.fixed !== true) {
+      if (vertical) {
+        $(grid).css('width', curr_height * (height/width));
+      } else {
+        $(grid).css('height', curr_width * (height/width));
+      }
+    };
+
+    layoutCells();
+  }
+
+  function layoutCells() {
+    var grid        = $.fn.gridly.opts.grid;
+    var rows        = $.fn.gridly.opts.rows;
+    var cols        = $.fn.gridly.opts.cols;
+    var width       = $.fn.gridly.opts.width;
+    var height      = $.fn.gridly.opts.height;
+    var curr_width  = $(grid).outerWidth();
+    var curr_height = $(grid).outerHeight();
+    var vertical    = ($.fn.gridly.opts.orientation === 'vertical');
+    var gutter      = $.fn.gridly.opts.gutter;
+
+    if (gutter > 0) {
+      curr_width = curr_width - (gutter * 2);
+      curr_height = curr_height - (gutter * 2);
+    }
+
+    var cell_width  = vertical ? (curr_width / rows) : (curr_width / cols);
+    var cell_height = vertical ? (curr_height / cols) : (curr_height / rows);
+    
+    if (gutter > 0) {
+      cell_width = cell_width - (gutter*2);
+      cell_height = cell_height - (gutter*2);
+    }
+
+    $(grid).find('.cell').each(function(){      
+      var x = parseInt($(this).data('cell-x'), 10) - 1;
+      var y = parseInt($(this).data('cell-y'), 10) - 1;
+      var w = parseInt($(this).data('cell-w') || 1, 10);
+      var h = parseInt($(this).data('cell-h') || 1, 10);
+
+      var topPos, leftPos, newWidth, newHeight;
+      var gutterWidth = gutter * 2;
+      if (gutter > 0) {
+        leftPos = gutterWidth;
+        if (x > 0) {
+          leftPos += x * (cell_width + gutterWidth);
+        }
+
+        newWidth = w * cell_width;
+        if (w > 1) {
+          newWidth += (w-1) * gutterWidth;
+        } 
+
+        topPos = gutterWidth;
+        if (y > 0) {
+          topPos += y * (cell_height + gutterWidth);
+        }
+        
+        newHeight = h * cell_height;
+        if (h > 1) {
+          newHeight += (h-1) * gutterWidth;
+        } 
+
+      } else {
+        topPos    = y * cell_height;
+        leftPos   = x * cell_width;
+        newWidth  = w * cell_width;
+        newHeight = h * cell_height;
+      }
+
+      $(this).css({
+        position: 'absolute',
+        top: topPos + 'px', 
+        left: leftPos + "px", 
+        width: newWidth + 'px', 
+        height: newHeight + "px"
+      })
+    });
+  }
+
+
+})(jQuery);
+
